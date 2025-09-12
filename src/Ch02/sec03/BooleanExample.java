2c4939280f1615a3b753871965cd7e8d633e19d3
KHS_GG <fksfksfn623@gmail.com>
2025-09-12 13:53:42 +0900

    Update BooleanExample.java

--- src/Ch02/sec03/BooleanExample.java
+++ src/Ch02/sec03/BooleanExample.java
@@ -5,7 +5,7 @@ public class BooleanExample {
 	public static void main(String[] args) {
 		boolean stop = true;
 		if(stop) {
-			System.out.println("즁지.");
+			System.out.println("중지.");
 		}else {
 			System.out.println("시작.");
 		}
